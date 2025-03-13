8e6d3e302d7ec01410fe2db99ac98a7157a06d0f
patriciagivort <63522907+patriciagivort@users.noreply.github.com>
2025-03-13 19:36:23 -0300

    Update ex28.py

--- ex28.py
+++ ex28.py
@@ -2,16 +2,19 @@
 # User try to guess the number the machine tough
 
 import random
+from time import sleep
 def lotto(x):
-
     sort_guess = random.randrange(1, 5)
     if x == sort_guess:
-        print(f'You won i think of the number {sort_guess}')
+        print(f'You won I was thinking the number {sort_guess}')
     else:
-        print(f'You lost i think of th enumber {sort_guess}')
+        print(f'You lost I was thinking the number {sort_guess}')
 
 print('=*'*20)
 print('Im going to think of a number from 1 to 5. Can you guess?')
 print('=*'*20)    
 user_guess = int(input('What number did i think off? '))
+print('Processing...')
+sleep(2)
+
 lotto(user_guess)
