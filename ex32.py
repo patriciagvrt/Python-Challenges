8c94a02f08886fafb13b8235c1c16cd0a759a6c9
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2025-03-14 18:22:58 -0300

    Update ex32.py
    upgrade exercice

--- ex32.py
+++ ex32.py
@@ -1,8 +1,11 @@
 # Create a program that reads a year and determines whether it is a leap year.
+from datetime import date
 def leapyear(x):
-    if x % 4 == 0 and x % 100 != 0:
+    if x == 0:
+       x = date.today().year
+    if x % 4 == 0 and x % 100 != 0 or x % 400 == 0:
         print(f'The year {x} is a leap year!')
     else:
         print(f'The year {x} is not a leap year')
-year = int(input('Type a year: '))
+year = int(input('Type a year, type 0 if you want the current year: '))
 leapyear(year)
