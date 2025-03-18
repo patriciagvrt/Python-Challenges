f487d96cdc0907b56eaccf23089e9a22e640231a
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2025-03-18 12:56:17 -0300

    new adds

---
+++ ex38.py
@@ -0,0 +1,17 @@
+# Write a program that reads two integer numbers and compares them,
+# displaying a message on the screen:
+#
+# - "The first value is greater" if the first number is larger.
+# - "The second value is greater" if the second number is larger.
+# - "There is no greater value, both are equal" if the numbers are the same.
+def greater_value():
+    n1 = int(input('Type the first number: '))
+    n2 = int(input('Type the second number: '))
+    if n1 > n2:
+        print('The first value is greater')
+    elif n2>n1:
+        print('The second value is greater')
+    else:
+        print(f'The two values are equal')
+greater_value()
+    
