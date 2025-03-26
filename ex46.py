040bdd61b4c0f2c15b41a1dc3e4c4e73f033c2a2
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2025-03-26 15:24:19 -0300

    exer

--- ex46.py
+++ ex46.py
@@ -1,2 +1,9 @@
 # Create a program that displays a countdown on the screen for a fireworks show,
 # counting from 10 to 0 with a 1-second pause between each number.
+from time import sleep
+def fireworks():
+    for i in range(10, 0, -1):
+        print(f'{i}!')
+        sleep(1)
+    print('BOOM!')
+fireworks()
--- ex47.py
+++ ex47.py
@@ -1,1 +1,9 @@
 # Create a program that displays all even numbers between 1 and 50.
+from time import sleep
+
+def even_number():
+    for i in range(2, 51, 2):  # Start at 2, go up to 50, step by 2
+        sleep(0.5)
+        print(f'{i}', end=' ')
+
+even_number()
