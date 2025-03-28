9112a67b4cecec47eb08e7de3d64dfe55e46f77e
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2025-03-28 19:21:28 -0300

    Update ex55.py

--- ex55.py
+++ ex55.py
@@ -1,2 +1,21 @@
 # Create a program that reads the weight of five people.
 # At the end, show which was the highest and the lowest weight recorded.
+
+def calculate_weight():
+
+    highest = 0
+    lowest = 0
+
+    for _ in range(5):
+        weight = float(input('Type the weight value(Kg):  '))
+        if weight > highest:
+            highest = weight
+            lowest = weight
+        elif weight < highest:
+            lowest = weight
+            if weight < lowest:
+                lowest = weight
+                
+    print(f'The highest value is {highest} the lowest value is {lowest}')
+
+calculate_weight()
