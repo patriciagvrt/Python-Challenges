ed2caa3298fb36f1e1d12425cace3063414b21fa
patriciagvrt <63522907+patriciagvrt@users.noreply.github.com>
2025-06-04 16:39:34 -0300

    Update ex46.py

--- ex46.py
+++ ex46.py
@@ -2,7 +2,7 @@
 # counting from 10 to 0 with a 1-second pause between each number.
 from time import sleep
 def fireworks():
-    for i in range(10, 0, -1):
+    for i in range(10, -1, -1):
         print(f'{i}!')
         sleep(1)
     print('BOOM!')
